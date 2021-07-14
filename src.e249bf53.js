94707bc2bd7c62d2c589932b88c1619c0b875568
AlexDvor <77227774+AlexDvor@users.noreply.github.com>
2021-07-14 19:56:42 +0000

    Deploying to gh-pages from @ AlexDvor/goit-js-hw-12@c3227b1e6f5e4d7da7b54a78ce3a759a1a422202 🚀

--- src.e249bf53.js
+++ src.e249bf53.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"krre":[function(require,module,exports) {
 
+},{}],"irxn":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var e={fetchCountries:t};function t(e){fetch(`https://restcountries.eu/rest/v2/name/${e}?fields=${e};name;capital;population;flag;languages`).then(e=>e.json()).then(console.log).catch(e=>console.log(e))}exports.default=e;
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./css/styles.css");const s=300;
-},{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-12/src.66961080.js.map
+"use strict";require("./css/styles.css");var e=t(require("./js/fetchCountries"));function t(e){return e&&e.__esModule?e:{default:e}}const r=300,s=document.querySelector("#search-box");function u(t){const r=t.currentTarget.value;e.default.fetchCountries(r)}s.addEventListener("input",u);
+},{"./css/styles.css":"krre","./js/fetchCountries":"irxn"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-12/src.e249bf53.js.map
