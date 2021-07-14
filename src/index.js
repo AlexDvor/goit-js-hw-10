c3227b1e6f5e4d7da7b54a78ce3a759a1a422202
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-07-14 21:56:13 +0200

    added js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 
 import './css/styles.css';
-
+import API from './js/fetchCountries';
+// // import './js/fetchApi';
 const DEBOUNCE_DELAY = 300;
+
+const inputValue = document.querySelector('#search-box');
+
+
+
+// ________________________________
+
+
+inputValue.addEventListener('input', onSearch)
+
+function onSearch(e) {
+    const formValue = e.currentTarget.value;
+ 
+    API.fetchCountries(formValue)
+    
+
+}
+
+
+
+// API.fetchCountries('spain')
+
+
+
