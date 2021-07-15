fb517100e406eff57157863ad48e444f6d1c30ff
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-07-15 23:32:59 +0200

    added js, css + html

--- src/index.js
+++ src/index.js
@@ -1,29 +1,74 @@
 
 import './css/styles.css';
 import API from './js/fetchCountries';
-// // import './js/fetchApi';
+import debounce from 'lodash.debounce'
+import countryCardTpl from './templates/card.hbs'
+import countryCardsListTpl from './templates/cards.hbs'
+
 const DEBOUNCE_DELAY = 300;
-
 const inputValue = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list')
 
+// // ________________________________
 
+inputValue.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-// ________________________________
-
-
-inputValue.addEventListener('input', onSearch)
 
 function onSearch(e) {
-    const formValue = e.currentTarget.value;
- 
-    API.fetchCountries(formValue)
+
+  const formValue = e.target.value.trim();
+
+  if (formValue === '') {
+    clearCards()
+    return
+  }
+
+  API.fetchCountries(formValue)
+    .then(renderCountryCard)
+    .catch(fetchError);
     
 
 }
 
 
+function renderCountryCard(data) {
+  
 
-// API.fetchCountries('spain')
+  if (data.length === 1) {
+  const card = countryCardTpl(data);
+  countryList.innerHTML = card;
+  }
+
+  if (data.length >= 2 && data.length <=10 ) {
+  const cards = countryCardsListTpl(data);
+  countryList.innerHTML = cards;
+  }
+  
+}
 
 
+function clearCards() {
+countryList.innerHTML = '';
+  
+}
+function fetchError(error) {
+  console.log(error)
+}
+
+
+
+
+
+// // __________________________________________
+
+// const listCountry = document.querySelector('.country-list')
+// console.log(listCountry)
+
+
+
+// function checkLength (arr) {
+//     if (arr.length > 10) {
+//         console.log('Введи конкретно страну', arr)
+//     }
+// }
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,12 @@
 
 export default { fetchCountries };
     
+
 function fetchCountries(name) {
-    const URL = 'https://restcountries.eu/rest/v2'
-    
-    fetch(`${URL}/name/${name}?fields=${name};name;capital;population;flag;languages`).then(r=> r.json()).then(console.log).catch(error => console.log(error))
-    
+  const URL = 'https://restcountries.eu/rest/v2'
+
+  return fetch(`${URL}/name/${name}?fields=${name};name;capital;population;flag;languages`)
+    .then(response => { return response.json() });
+
 }
-
-
-
-
 
